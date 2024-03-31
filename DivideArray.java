01dddb456f8d1787991966b8fe776211da499c07
Sonu Jaiswal <163344193+sonujaiswal450@users.noreply.github.com>
2024-04-01 00:02:58 +0530

    Update DivideArray.java

--- DivideArray.java
+++ DivideArray.java
@@ -40,3 +40,20 @@ public class DivideArray {
         System.out.println("]");
     }
 }
+/*output
+Let consider a array with size 9.
+Enter the size of the array :9
+Enter the element into the array :
+Element1 :5
+Element2 :9
+Element3 :3
+Element4 :5
+Element5 :7
+Element6 :2
+Element7 :1
+Element8 :12
+Element9 :4
+arr1: [5, 9, 4]
+arr2: [9, 2, 12]
+arr3: [3, 7, 1]
+middle element of every sub array is random element from the parent array elements.*/
